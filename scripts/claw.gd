74d50fba1e8b61c34f10fa688b2f8f87f7310b59
Rachel <153067117+tuinkers1@users.noreply.github.com>
2025-01-24 21:15:55 +0100

    da movement

---
+++ scripts/claw.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var speed = 250
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	velocity = velocity * speed
+	position += velocity * delta
