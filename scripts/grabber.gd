952ade1b0da0065be8a354ff0c7d4f790166be80
Rachel <153067117+tuinkers1@users.noreply.github.com>
2025-01-25 15:28:59 +0100

    cool short fix

--- scripts/grabber.gd
+++ scripts/grabber.gd
@@ -10,6 +10,11 @@ var claw
 var destination = Vector2.ZERO
 var moves_to_make = 0
 var claw_timer_running = false
+var up = false
+var just_attached = false
+var left_timer
+var right_timer
+var almost_detach = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,33 +30,70 @@ func _ready() -> void:
 		moves_to_make = claw_down_distance / claw_speed
 		self.global_position
 		attached = false
+		
+
+	
+		
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("grab"):
+	if Input.is_action_just_pressed("grab") && moves_to_make == 0:
+		#print("yes")
+		#code for birnging claw down
+		#move the arm down
+		#amount of moves to make
+		moves_to_make = claw_down_distance / claw_speed
+		self.global_position
+	if Input.is_action_pressed("left") && Input.is_action_pressed("right") && moves_to_make == 0:
 		print("yes")
 		#code for birnging claw down
 		#move the arm down
 		#amount of moves to make
 		moves_to_make = claw_down_distance / claw_speed
 		self.global_position
-		attached = false
+
+
 	
 	if moves_to_make != 0 && claw_timer_running == false:
 		claw_timer_running = true
-		await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(0.60).timeout
 		claw_timer_running = false
-		print("gay")
-		claw.global_position.y += claw_speed
+		#print("gay")
+		if up == false:
+			claw.global_position.y += claw_speed
+		if up == true:
+			claw.global_position.y -= claw_speed
+		
+		if moves_to_make == 0 && up==false:
+			if just_attached == false:
+				if almost_detach == true:
+					attached = false
+		
+		print(moves_to_make)
 		moves_to_make -= 1
+		if moves_to_make == 0 && up==false:
+			if just_attached == false:
+				almost_detach = true
+				attached = false
+				
+			turn_around()
+		if moves_to_make == 0 && up == true:
+			up = false
+			just_attached = false
 		
 	if attached == true:
 		object_on_claw.global_position = self.global_position
 
 
-
+func turn_around():
+	up = true
+	moves_to_make = claw_down_distance / claw_speed
+	pass
 
 func _on_grabable_area_area_entered(area: Area2D) -> void:
 	object_on_claw = bubble_1
 	attached = true
+	just_attached = true
