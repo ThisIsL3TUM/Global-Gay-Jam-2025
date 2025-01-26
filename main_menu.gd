a5ab99b21814d2b8dbfeec34dbfa914915e6bd50
Beau Baghiu <bs.baghiu@st.hanze.nl>
2025-01-26 11:39:27 +0100

    MAIN MENU
    -> made the main menu

---
+++ main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	if event is InputEventKey:
+		if Input.is_action_pressed("right") and Input.is_action_pressed("left"):
+			get_tree().change_scene_to_file("res://crane_testing.tscn")
